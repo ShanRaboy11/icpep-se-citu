7c7748223aa9e2c3c920e71bfa98e7193f512897
Gio Christian D. Macatual <giochristian04@gmail.com>
2025-10-16 02:29:43 +0800

    feat(login_page):implemented animation - Macatual

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,13 +1,11 @@
 'use client';
 
 import LandingPage from "./pages/home/landing-page";
-import Login from "./pages/home/login";
 
 export default function App() {
   return (
     <div>
     <LandingPage />
-      <Login/>
     </div>
   );
 }
