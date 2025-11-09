859e211de52e101b9c990bab2a47d9e3d2063534
Gio Christian D. Macatual <giochristian04@gmail.com>
2025-11-09 23:23:39 +0800

    style:styling - Macatual

--- client/src/app/profile/components/password.tsx
+++ client/src/app/profile/components/password.tsx
@@ -4,24 +4,23 @@ import Button from "@/app/components/button";
 
 export default function SecuritySection() {
   return (
-    <div className="w-full border border-primary1 rounded-lg p-5 px-7 bg-white shadow-sm">
-      <h2 className="text-xl font-rubik text-black font-semibold mb-4 border-b border-primary1 pb-2">
+    <div className="w-full border border-primary1 rounded-lg p-5 px-7 sm:px-10 sm:py-8 bg-white shadow-sm">
+      <h2 className="text-xl sm:text-2xl font-rubik text-black font-semibold mb-4 border-b border-primary1 pb-2 sm:mr-5">
         Security
       </h2>
 
       <div className="flex flex-row justify-between items-center text-sm md:text-base">
-        <div className="flex items-center gap-3 whitespace-nowrap text-black font-raleway text-lg">
-          <Lock size={18} />
+        <div className="flex items-center gap-3 whitespace-nowrap text-black font-raleway text-lg ">
           <span>Password</span>
         </div>
 
-        <span className="text-xs text-gray-400 hidden sm:inline">
+        <span className="text-s text-gray-400 hidden sm:inline">
           Last changed 1 month ago
         </span>
 
         <Button
           variant="primary2"
-          className="flex items-center gap-2 whitespace-nowrap"
+          className="flex items-center gap-2 whitespace-nowrap sm:mr-10"
         >
           Update
         </Button>
